40f4169083691736f03310f07d87f36cef75c70c
André Alves <andreluialves@hotmail.com>
2022-05-26 10:43:57 -0300

    feat: criada a possibilidade de seleção do item, alterando o background

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const ordenedList = document.getElementById('list-tasks');
 const inputButton = document.getElementById('create-task');
+let itensArray;
+let item;
 let taskItem;
 // let itemListFull;
 
@@ -14,3 +16,35 @@ function createItem() {
 }
 
 inputButton.addEventListener('click', createItem);
+
+// Add background task item
+function itemBgClick(event) {
+  for (let index = 0; index < item.length; index += 1) {
+    itensArray = item[index];
+    itensArray.style.backgroundColor = 'white';
+    itensArray.classList.remove('selected');
+  }
+  event.target.style.backgroundColor = 'rgb(128 128 128)';
+  event.target.classList.add('selected');
+}
+
+// Crossed off when double-clicked
+function crossedOff(event) {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+}
+
+// Add event listener in the all task items
+function forItens() {
+  item = document.querySelectorAll('#list-tasks li');
+  for (let index = 0; index < item.length; index += 1) {
+    itensArray = item[index];
+  }
+  itensArray.addEventListener('click', itemBgClick);
+  itensArray.addEventListener('dblclick', crossedOff);
+}
+
+inputButton.addEventListener('click', forItens);
